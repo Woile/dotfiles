7d7354436b059e3b6fe721fc2ab8c2d7a6ae5adb
Santiago Fraire Willemoës <santiwilly@gmail.com>
2019-02-20 22:29:53 +0100

    feat: kubernetes context + namespace

--- extra/prompt_config.bash
+++ extra/prompt_config.bash
@@ -6,11 +6,16 @@
 # Disable default virtualenv prompt
 export VIRTUAL_ENV_DISABLE_PROMPT=1
 
+# CONFIG
+BASH_KUBE="${BASH_KUBE:-true}"
 
 BASH_THEME_VIRTUAL_ENV_PROMPT_PREFIX="("
 BASH_THEME_VIRTUAL_ENV_PROMPT_SUFFIX=")"
 BASH_THEME_NODEJS_PROMPT_PREFIX="{"
 BASH_THEME_NODEJS_PROMPT_SUFFIX="}"
+BASH_THEME_KUBE_PROMPT_PREFIX="["
+BASH_THEME_KUBE_PROMPT_SUFFIX="]"
+
 
 _get_virtualenv_prompt() {
     if [ -n "$VIRTUAL_ENV" ]; then
@@ -34,10 +39,22 @@ _get_nodejs_prompt() {
     fi
 }
 
+_get_kube_prompt() {
+    if [[ "${BASH_KUBE}" == true ]]; then
+        if hash kubectl 2>/dev/null;then
+            local namespace=$(kubectl config view --minify --output 'jsonpath={..namespace}')
+            local current_context=$(kubectl config current-context)
+        fi
+        if [ "$namespace" != "" ]; then
+            echo "${BASH_THEME_KUBE_PROMPT_PREFIX}${current_context}:${namespace}${BASH_THEME_KUBE_PROMPT_SUFFIX}"
+        fi
+    fi
+}
+
 python_interpreter_like_prompt() {
     # (Virtualenv)user@host in ~/working_path |git_branch_status|
 
-    PS1="${YELLOW_B}$(_get_virtualenv_prompt)${GREEN}$(_get_nodejs_prompt)${GREEN_B}\u${BLUE_B}@${RED_B}\h${WHITE_B} in ${BLUE_B}\w${CYAN} $(__git_ps1 "|%s|")${GREEN_B} $\n${GREEN_B}>>>\[\033[00m\] "
+    PS1="${YELLOW_B}$(_get_virtualenv_prompt)${GREEN}$(_get_nodejs_prompt)${BLUE_B}$(_get_kube_prompt)${GREEN_B}\u${BLUE_B}@${RED_B}\h${WHITE_B} in ${BLUE_B}\w${CYAN} $(__git_ps1 "|%s|")${GREEN_B} $\n${GREEN_B}>>>\[\033[00m\] "
 }
 
 # Set the prompt
